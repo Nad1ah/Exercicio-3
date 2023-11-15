2f8185c640558a4f3a612b6ea90300834f6424c7
Nadiah Mauricio <nadiahmauricio@gmail.com>
2023-11-15 20:14:20 +0000

    end exercicio

--- exercicio2/app.ts
+++ exercicio2/app.ts
@@ -8,15 +8,21 @@ const computerNumber = getRandom(0, 100);
 
 console.log(`Número escolhido: ${computerNumber}`);
 
-const userChoice = Number.parseInt(prompt("Escolha um número"));
-const isInvalid = Number.isNaN(userChoice);
+let turnCount = 1;
+while (turnCount <= 10) {
+  const userChoice = Number.parseInt(prompt("Escolha um número"));
+  prompt(`Turno ${turnCount}: Escolha um número`);
+  const isInvalid = Number.isNaN(userChoice);
 
-if (isInvalid) {
-  console.log("Número Inválido");
-} else if (userChoice > computerNumber) {
-  console.log("Demasiado alto");
-} else if (userChoice < computerNumber) {
-  console.log("Demasiado baixo");
-} else {
-  console.log("Certo, acertaste!");
+  if (isInvalid) {
+    console.log("Número Inválido");
+  } else if (userChoice > computerNumber) {
+    console.log("Demasiado alto");
+  } else if (userChoice < computerNumber) {
+    console.log("Demasiado baixo");
+  } else {
+    console.log("Certo, acertaste!");
+  }
+
+  turnCount <= 1;
 }
